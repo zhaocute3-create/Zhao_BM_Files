893cb4ac8d7658aea0d862076f4cc28682282750
ZhaoShop <zhaocute3@gmail.com>
2026-04-18 23:05:08 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
-// 🔥 Firebase config (PALITAN MO)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyxbJtIGOfiU5_h8BRKsXyK4RC_wIET3s",
-  authDomain: "zhaobmfiles.firebaseapp.com",
-  projectId: "zhaobmfiles",
-  storageBucket: "zhaobmfiles.firebasestorage.app",
-  messagingSenderId: "898227903245",
-  appId: "1:898227903245:web:c21f3fa479b13c7971ae44"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
