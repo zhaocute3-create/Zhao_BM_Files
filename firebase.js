8a373cce52b462ce7f1ec0c17223f164626addda
ZhaoShop <zhaocute3@gmail.com>
2026-04-18 19:51:45 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,9 +3,9 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyAyxbJtIGOfiU5_h8BRKsXyK4RC_wIET3s",
+  authDomain: "zhaobmfiles.firebaseapp.com",
+  projectId: "zhaobmfiles",
 };
 
 const app = initializeApp(firebaseConfig);
